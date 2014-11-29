bb0b9adb63fe5deb802b1da2e5f1894c7843f558
Jan Müller <jan@Jans-MacBook.fritz.box>
2014-11-29 17:25:51 +0100

    Create Database if not exists on construction

--- Core/ADO/Database.cs
+++ Core/ADO/Database.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Mono.Data.Sqlite;
+using System.IO;
+using System.Data;
 
 namespace Core
 {
@@ -7,8 +13,43 @@ namespace Core
 	/// </summary>
 	public class Database
 	{
-		public Database ()
+		static object _dblock = new object ();
+
+		private SqliteConnection connection;
+
+		private string path;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Database"/> Database.
+		/// If the database doesn't exist, it will create the database and all the tables.
+		/// </summary>
+		public Database (string dbPath)
 		{
+			var output = "";
+			path = dbPath;
+
+			bool exists = File.Exists (dbPath);
+
+			if (!exists) {
+				connection = new SqliteConnection ("Data Source=" + path);
+
+				connection.Open ();
+				var commands = new[] {
+					"CREATE TABLE [Projects] (_id INTEGER PRIMARY KEY ASC, " +
+					"description NTEXT, " +
+					"created_at DATETIME);",
+					"CREATE TABLE [Records] (_id INTEGER PRIMARY KEY ASC, " +
+					"project_id INTEGER, " +
+					"start_time DATETIME, end_time DATETIME)"
+				};
+				foreach (var command in commands) {
+					using (var c = connection.CreateCommand ()) {
+						c.CommandText = command;
+						c.ExecuteNonQuery ();
+					}
+				}
+			}
+			Console.WriteLine (output);
 		}
 	}
 }
