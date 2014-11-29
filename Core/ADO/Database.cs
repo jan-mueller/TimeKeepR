03a3260a4cbe031f37f93ba1728f6df3b449af82
Jan Müller <jan@Jans-MacBook.fritz.box>
2014-11-29 16:46:26 +0100

    Create Database class

---
+++ Core/ADO/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core
+{
+	/// <summary>
+	/// Database uses ADO.NET to create the [Items] table and create,read,update,delete data
+	/// </summary>
+	public class Database
+	{
+		public Database ()
+		{
+		}
+	}
+}
+
