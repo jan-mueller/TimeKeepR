206b76aa2cc107691b25c03d1178fc75ee72e178
Jan Müller <jan@Jans-MacBook.fritz.box>
2014-11-29 16:39:39 +0100

    Initial Commit

---
+++ Core/Types/Project.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Core
+{
+	/// <summary>
+	/// Project business object
+	/// </summary>
+	public class Project
+	{
+		#region members
+		private uint id;
+
+		private string description;
+
+		private DateTime createdAt;
+		#endregion
+
+		#region construction
+		public Project ()
+		{
+		}
+		#endregion
+
+		#region properties
+		public uint Id {
+			get {
+				return id;
+			}
+			set {
+				id = value;
+			}
+		}
+			
+		public string Description {
+			get {
+				return description;
+			}
+			set {
+				description = value;
+			}
+		}
+
+		public DateTime CreatedAt {
+			get {
+				return createdAt;
+			}
+			set {
+				createdAt = value;
+			}
+		}
+		#endregion
+	}
+}
+
---
+++ Core/Types/Record.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Core
+{
+	/// <summary>
+	/// Record business object
+	/// </summary>
+	public class Record
+	{
+		#region members
+		private uint id;
+
+		private uint projectId;
+		
+		private string[] tags;
+
+		private DateTime startTime;
+
+		private DateTime endTime;
+		#endregion
+
+		#region construction
+		public Record ()
+		{
+		}
+		#endregion
+
+		#region properties
+		public uint ProjectId {
+			get {
+				return projectId;
+			}
+			set { 
+				this.projectId = value;
+			}
+		}
+
+		public string[] Tags {
+			get {
+				return tags;
+			}
+			set { 
+				this.tags = value;
+			}
+		}
+
+		public DateTime StartTime {
+			get {
+				return startTime;
+			}
+			set {
+				startTime = value;
+			}
+		}
+
+		public DateTime EndTime {
+			get {
+				return endTime;
+			}
+			set {
+				endTime = value;
+			}
+		}
+		#endregion
+	}
+}
+
